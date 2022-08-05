fd2e8b6467d1725b6b5eda41b94fe99d3bd25cd2
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-08-05 18:06:15 +0200

    useref on login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,17 +1,27 @@
 import Styles from "./login.module.css";
 import { XIcon } from "@heroicons/react/solid";
+import { useEffect, useRef } from "react";
 
 interface Props {
   setToogleLogin: (toogle: boolean) => void;
 }
 
 const Login: React.FC<Props> = ({ setToogleLogin }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
   return (
     <div className={Styles.container}>
       <XIcon onClick={() => setToogleLogin(false)} className={Styles.xIcon} />
       <form className={Styles.form} action=''>
         <label htmlFor=''>Username:</label>
-        <input className={Styles.input} type='text' placeholder='Username' />
+        <input
+          ref={inputRef}
+          className={Styles.input}
+          type='text'
+          placeholder='Username'
+        />
         <label htmlFor=''>Password</label>
         <input className={Styles.input} type='text' placeholder='Password' />
         <button>Login</button>
