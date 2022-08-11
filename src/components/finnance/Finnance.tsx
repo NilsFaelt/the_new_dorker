85681d73b4cb899336f4300b8b7731fcac73fe6b
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-08-11 18:25:14 +0200

    some stoc css

--- src/components/finnance/Finnance.tsx
+++ src/components/finnance/Finnance.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PopUp from "../popUp/PopUp";
 import Stock from "../stock/Stock";
 import Styles from "./finnance.module.css";
@@ -24,31 +25,43 @@ interface Props {
 }
 
 const Finnance: React.FC<Props> = ({ stockGainers, stocks }) => {
+  const [toogle, setToogle] = useState<boolean>(true);
   console.log(stockGainers);
   return (
-    <div className={Styles.container}>
-      <PopUp />
-      <div className={Styles.stockContainer}>
-        {stocks?.map((stock) => (
-          <Stock stock={stock} />
-        ))}
+    <div>
+      <nav className={Styles.nav}>
+        <ul className={Styles.ul}>
+          <li onClick={() => setToogle(true)}>Top 100</li>
+          <li onClick={() => setToogle(false)}>Todays News</li>
+        </ul>
+        <hr />
+      </nav>
+      <div className={Styles.container}>
+        <PopUp />
+        {toogle ? (
+          <div className={Styles.stockContainer}>
+            {stocks?.map((stock) => (
+              <Stock stock={stock} />
+            ))}
+          </div>
+        ) : null}
+        <aside className={Styles.aside}>
+          <h3 className={Styles.gainsTitle}>Todays Gainers</h3>
+          {stockGainers?.map((stock) => {
+            return (
+              <div className={Styles.gainersDiv}>
+                <p style={{ textDecoration: "underline" }}>
+                  Ticker:{stock.symbol}
+                </p>
+                <p style={{ fontFamily: "Cinzel, serif" }}>{stock.name}</p>
+                <p style={{ color: "green" }}>{stock.price} $</p>
+                <p style={{ color: "green" }}>{stock.changesPercentage} %</p>
+                <hr />
+              </div>
+            );
+          })}
+        </aside>
       </div>
-      <aside className={Styles.aside}>
-        <h3 className={Styles.gainsTitle}>Todays Gainers</h3>
-        {stockGainers?.map((stock) => {
-          return (
-            <div className={Styles.gainersDiv}>
-              <p style={{ textDecoration: "underline" }}>
-                Ticker:{stock.symbol}
-              </p>
-              <p style={{ fontFamily: "Cinzel, serif" }}>{stock.name}</p>
-              <p style={{ color: "green" }}>{stock.price} $</p>
-              <p style={{ color: "green" }}>{stock.changesPercentage} %</p>
-              <hr />
-            </div>
-          );
-        })}
-      </aside>
     </div>
   );
 };
